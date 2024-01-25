f221ca9db0b0593f25059578c36536aeda4511a2
tornike-topuria <147945276+tornike-topuria@users.noreply.github.com>
2024-01-25 21:31:32 +0400

    end

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 //1
+let array1 =[ 2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8]
+function dadebitiricxvebi(arr) {
+     sum = 0;
 
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            sum += arr[i];
+        }
+    }
+
+    return sum;
+}
+
+ result = dadebitiricxvebi(array1);
+console.log(result);
+
+
+
+ 
 
 
 
@@ -22,22 +40,61 @@ let user = {
     age: 32,
     isloggedin: true
   }
-
-  //5 
-  let array5 = [5, 6, 8, 9]
-function findEven(arr){
-for(let i = 0; i < arr.length; i++){
-    if(arr[i] % 2 === 0){
-        console.log(arr[i] + "this number is even")
+  function True() {
+    if (user.isloggedin == true){
+        console.log(user.firstname, user.lastname); 
     }
     else{
-        console.log(arr[i] + "this number is odd")
+        console.log(false);
+    }    
+}
+ True()
+
+ //4
+let array4 = [-4, -20, 12]
+function Number(hig) {
+    for (i = 0; i < hig.length; i++) {
+        if(hig[i] > hig.length) {
+            console.log(hig[i]);
+        }
     }
 }
+Number(array4)
+
+ 
+
+  //5 
+  function eveN(NumbeR) {
+    NumbeR = 20
+    if(NumbeR % 2 === 0){
+       console.log("this number is even");
+   }
+   
+     else{  console.log("this number is odd");
+    }
+   }
+   eveN()
+
+   //6
+ let array6 = [1, 2, 3, 4, 5];
+ for(i = array6.length - 1; i >= 0 ; i--){
+    console.log(array6[i])
+
+ }
+
+
+
+//7
+function age(){
+    Age = 20
+    if(Age > 18){
+        console.log("სრულწლოვანი");
+    }
+    else{
+        console.log( "არასწულწლოვანი");
+    }
 }
-findEven(array5)
-
-
+age()
 
 
 
@@ -45,9 +102,15 @@ findEven(array5)
 //8
 function array8(num1,num2){
     
-    
-    return num1 > num2
+    if(num1 > num2) {
+        console.log("num1 is the largest");
+    }
+    else if(num2 > num1) {
+        console.log("num2 is the largest");
+    }
+    else{
+        console.log("error");
+    }
       
 }
- result = array8(9 , 7)
-console.log(result);
+array8(9 , 7)
